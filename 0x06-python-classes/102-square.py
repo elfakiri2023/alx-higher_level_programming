0bb36753dfc860d759bc56decdcbf522ee7a5e4c
Mohamed Elfakiri <elfakiri2021@gmail.com>
2023-11-21 08:50:24 -0800

    Updating 102-square.py

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""My sqodule"""
+
+
+class Square:
+    """defineuare"""
+
+    def __init__(self, size=0):
+        """Createuare
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """"The propery ofa side of Square
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
+
+    def area(self):
+        """Ge
+        """
+        return self.__size * self.__size
+
+    def __le__(self, other):
+        return self.area() <= other.area()
+
+    def __lt__(self, other):
+        return self.area() < other.area()
+
+    def __ge__(self, other):
+        return self.area() >= other.area()
+
+    def __ne__(self, other):
+        return self.area() != other.area()
+
+    def __gt__(self, other):
+        return self.area() > other.area()
+
+    def __eq__(self, other):
+        return self.area() == other.area()
