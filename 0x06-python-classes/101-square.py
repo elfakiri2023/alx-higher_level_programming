da1b39a4a4b8a7d44fdeb570562dbd78a9934799
Mohamed Elfakiri <elfakiri2021@gmail.com>
2023-11-21 08:49:17 -0800

    Updating 101-square.py

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -1,58 +1,75 @@
 #!/usr/bin/python3
-"""
-Tprint out the square.
-"""
+"""my module."""
 
 
 class Square:
-    """A classffset on top and left.
-    """
+    """deare."""
+
+    def __str__(self):
+        """teach pytre my way"""
+        return self.pos_print()[:-1]
+
     def __init__(self, size=0, position=(0, 0)):
+        """ initialize the square with this
+        """
         self.size = size
         self.position = position
 
     @property
     def size(self):
+        """property of the length of a side of square
+        """
         return self.__size
 
     @size.setter
-    def size(self, size):
-        if type(size) != int:
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
+    def size(self, value):
+        """ set the size of square
+        """
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
 
     @property
     def position(self):
+        """property of the position of square
+        """
         return self.__position
 
     @position.setter
     def position(self, value):
-        if type(value) != tuple or len(value) != 2 or \
-           not all([type(i) == int for i in value]):
-            raise TypeError("position must be a tuple of 2 positive integers")
+        """set the position
+        """
+        if not isinstance(value, tuple):
+            raise TypeError('position must be a tuple of 2 positive integers')
+        if len(value) != 2:
+            raise TypeError('position must be a tuple of 2 positive integers')
+        if len([i for i in value if isinstance(i, int) and i >= 0]) != 2:
+            raise TypeError('position must be a tuple of 2 positive integers')
         self.__position = value
 
-    def __repr__(self):
-        return (self.get_str())
-
     def area(self):
+        """ th
+            size * size
+        """
         return self.__size * self.__size
 
-    def get_str(self):
-        total = ""
-        if self.__size is 0:
-            total += "\n"
-            return total
-        for i in range(self.__position[1]):
-            total += "\n"
-        for i in range(self.__size):
-            total += (" " * self.__position[0])
-            total += ("#" * self.__size)
-            if i is not (self.__size - 1):
-                total += "\n"
-        return total
+    def pos_print(self):
+        """retth position"""
+        pos = ""
+        if not self.size:
+            return "\n"
+        for w in range(self.position[1]):
+            pos += "\n"
+        for w in range(self.size):
+            for i in range(self.position[0]):
+                pos += " "
+            for j in range(self.size):
+                pos += "#"
+            pos += "\n"
+        return pos
 
     def my_print(self):
-        print(self.get_str())
+        """prre."""
+        print(self.pos_print(), end="")
