25eb645daaf2b035e63e15a892f5824ff6b716ce
Mohamed Elfakiri <elfakiri2021@gmail.com>
2023-12-04 14:34:24 -0800

    Updating 6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a ometry."""
+
+
+class BaseGeometry:
+    """this clometry"""
+
+    def area(self):
+        """methed yet"""
+        raise Exception("area() is not implemented")
