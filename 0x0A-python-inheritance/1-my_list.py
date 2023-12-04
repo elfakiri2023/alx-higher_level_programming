1219ab0ff375ddf19981bb86d17a2dd99aaad6a8
Mohamed Elfakiri <elfakiri2021@gmail.com>
2023-12-04 06:49:46 -0800

    Updating 1-my_list.py

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module inist class"""
+
+
+class MyList(list):
+    """A class throm list"""
+    def print_sorted(self):
+        """prints list"""
+        print(sorted(self))
