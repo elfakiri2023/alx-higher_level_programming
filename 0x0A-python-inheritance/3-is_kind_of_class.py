015eba63ca5a917beb991647611c91edecf11390
Mohamed Elfakiri <elfakiri2021@gmail.com>
2023-12-04 14:31:16 -0800

    Updating 3-is_kind_of_class.py

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""checks if oss
+or an inh
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """returns tass
+    or a class th
+    """
+    return (isinstance(obj, a_class))
