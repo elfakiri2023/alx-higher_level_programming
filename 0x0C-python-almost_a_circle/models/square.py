04551bf8efbb2410182c7e928b9443bead59b2fe
Mohamed <137074672+elfakiri2023@users.noreply.github.com>
2023-12-12 02:03:54 +0100

    SSH not working

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+
+""" This module defines a Square Class"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """
+        Class Square
+    """
+
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
+
+    def __str__(self):
+        """ This module defines a Square Class"""
+        return (
+                "[Square] ({}) {}/{} - {}".format(
+                    self.id, self.x, self.y, self.width))
+
+    def update(self, *args, **kwargs):
+        """ This module defines a Square Class"""
+        if args:
+            attributes = ["id", "size", "x", "y"]
+            for i in range(len(args)):
+                setattr(self, attributes[i], args[i])
+        elif kwargs:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
+
+    def to_dictionary(self):
+        """ This module defines a Square Class"""
+        return {
+            'id': self.id, 'size': self.width,
+            'x': self.x, 'y': self.y}
---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""Module for Square unittest"""
+import unittest
+from models.base import Base
+from models.square import Square
+
+
+class TestSquare(unittest.TestCase):
+    """Test Base Class"""
+
+    def setUp(self):
+        Base.__nb_objects = 0
+        pass
+
+    def test_class(self):
+        """Test for Square Class"""
+        self.assertEqual(str(
+            Square), "<class 'models.square.Square'>")
+    
+    def test_inheritance(self):
+        """Test inheritance Square->Base"""
+        self.assertTrue(issubclass(Square, Base))
+    
+    def test_constructor(self):
+        """Test constructor"""
+        with self.assertRaises(TypeError) as e:
+            r = Square()
+    
+    def test_constructor_args(self):
+        """Test constructor"""
+        with self.assertRaises(TypeError) as e:
+            r = Square(1, 2, 5, 8, 9)
+        s = "__init__() takes from 2 to 5 positional arguments but 6 \
+were given"
+
+        self.assertEqual(str(e.exception), s)
+
+if __name__ == '__main__':
+    unittest.main()
