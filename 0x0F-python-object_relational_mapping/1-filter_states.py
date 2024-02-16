ea7ca823487e367d9a1590b27d2e52e898ba0443
Mohamed Elfakiri <elfakiri2021@gmail.com>
2024-02-16 14:23:11 -0800

    Updating 1-filter_states.py

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+lists all states with
+starting with the letter `N`
+"""
+
+import MySQLdb as db
+from sys import argv
+
+"""
+lists all states with
+starting with the letter `N`
+"""
+
+if __name__ == '__main__':
+    db_connect = db.connect(host="localhost", port=3306,
+                            user=argv[1], passwd=argv[2], db=argv[3])
+    db_cursor = db_connect.cursor()
+
+    db_cursor.execute(
+        "SELECT * FROM states WHERE name LIKE BINARY 'N%' \
+                ORDER BY states.id ASC")
+
+    rows_selected = db_cursor.fetchall()
+
+    for row in rows_selected:
+        print(row)
