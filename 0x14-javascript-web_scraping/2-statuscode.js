5a9f50b21a27fc8fd445c1502e4922cddc134748
Mohamed Elfakiri <elfakiri2021@gmail.com>
2024-03-27 23:18:15 +0000

    Updating 1-writeme.js

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,10 +2,10 @@
 const request = require('request');
 const url = process.argv[2];
 
-request.get(url, (error, response) => {
+request.get(url, (error, res) => {
   if (error) {
     console.log(error);
   } else {
-    console.log(`code: ${response.statusCode}`);
+    console.log('code:', res.statusCode);
   }
 });
