9b7f2da59a260bd4ab9c689230d7cb7512767c1c
Mohamed Elfakiri <elfakiri2021@gmail.com>
2024-03-27 23:21:13 +0000

    Updating 0-readme.js

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,9 +3,9 @@ const request = require('request');
 const url = process.argv[2];
 
 request.get(url, (error, res) => {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log('code:', res.statusCode);
-  }
+	if (error) {
+		console.log(error);
+	} else {
+		console.log('code:', res.statusCode);
+	}
 });
--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -6,10 +6,10 @@ const url = 'https://swapi-api.alx-tools.com/api/films';
 const id = process.argv[2];
 
 request.get(`${url}/${id}`, (error, res, body) => {
-  if (error) {
-    console.log(error);
-  } else {
-    const data = JSON.parse(body);
-    console.log(data.title);
-  }
+	if (error) {
+		console.log(error);
+	} else {
+		const data = JSON.parse(body);
+		console.log(data.title);
+	}
 });
