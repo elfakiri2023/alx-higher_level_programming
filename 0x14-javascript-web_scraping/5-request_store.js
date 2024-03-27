186bd71d4b313b788f48b1a3d989540fd6829664
Mohamed Elfakiri <elfakiri2021@gmail.com>
2024-03-27 23:23:57 +0000

    Updating 5-request_store.js

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -2,17 +2,18 @@
 
 const request = require('request');
 const fs = require('fs');
+
 const url = process.argv[2];
 const file = process.argv[3];
 
-request(url, (error, response, body) => {
-  if (error) {
-    console.log(error);
-  } else {
-    fs.writeFile(file, body, 'utf8', (error) => {
-      if (error) {
-        console.log(error);
-      }
-    });
-  }
+request.get(url, (error, res, body) => {
+	if (error) {
+		console.log(error);
+	} else {
+		fs.writeFile(file, body, 'utf-8', (error) => {
+			if (error) {
+				console.log(error);
+			}
+		});
+	}
 });
