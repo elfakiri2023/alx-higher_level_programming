e0a9faa6cd73fc6c42cb58e49ea48bea7d75c5dc
Mohamed Elfakiri <elfakiri2021@gmail.com>
2024-03-27 23:59:42 +0000

    Updating 6-completed_tasks.js

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -3,19 +3,24 @@
 const request = require('request');
 const url = process.argv[2];
 
-request.get(url, { json: true }, (error, response, body) => {
-	if (error) {
-		console.log(error);
-	}
-	const Completedtasks = {};
-	body.forEach((todo) => {
-		if (todo.completed) {
-			if (!Completedtasks[todo.userId]) {
-				Completedtasks[todo.userId] = 1;
-			} else {
-				Completedtasks[todo.userId] += 1;
-			}
-		}
-	});
-	console.log(Completedtasks);
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    const completed = {};
+    const tasks = JSON.parse(body);
+    for (const i in tasks) {
+      const task = tasks[i];
+      if (task.completed === true) {
+        if (completed[task.userId] === undefined) {
+          completed[task.userId] = 1;
+        } else {
+          completed[task.userId]++;
+        }
+      }
+    }
+    console.log(completed);
+  } else {
+    console.log('An error occured. Status code: ' + response.statusCode);
+  }
 });
