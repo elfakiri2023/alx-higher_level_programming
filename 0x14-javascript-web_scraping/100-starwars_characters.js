5aaf697fdb08f8b573fe07c949f84fdccf1f65e5
Mohamed Elfakiri <elfakiri2021@gmail.com>
2024-03-27 23:26:49 +0000

    Updating 100-starwars_characters.js

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -3,24 +3,22 @@
 const request = require('request');
 
 const movieId = process.argv[2];
-const url = `https://swapi.dev/api/films/${movieId}/`;
+const url = 'https://swapi-api.alx-tools.com/api/films';
 
-request.get(url, (error, response, body) => {
-  if (error) {
-    console.log(error);
-    return;
-  }
+request.get(`${url}/${movieId}/`, (error, res, body) => {
+	if (error) {
+		console.log(error);
+	}
 
-  const data = JSON.parse(body);
-  const characters = data.characters;
-  for (const character of characters) {
-    request(character, (error, response, body) => {
-      if (error) {
-        console.log(error);
-        return;
-      }
-      const characterData = JSON.parse(body);
-      console.log(characterData.name);
-    });
-  }
+	const data = JSON.parse(body);
+	const characters = data.characters;
+	for (const character of characters) {
+		request(character, (error, res, body) => {
+			if (error) {
+				console.log(error);
+			}
+			const characterdata = JSON.parse(body);
+			console.log(characterdata.name);
+		});
+	}
 });
